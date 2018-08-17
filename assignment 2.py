0a83272c1dfa9656bf4e9113713c5ed4457cfe95
tanuj07 <32153574+tanuj07@users.noreply.github.com>
2018-08-17 17:33:02 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,27 @@
+#Question-1
+print("Hi,I am using pycharm")
+
+#Question 2
+print("Tanuj"+" "+"Kumar")
+
+#Question 3
+x=int(input("enter x"))
+y=int(input("enter y"))
+z=int(input("enter z"))
+print("value of x:",x)
+print("value of y:",y)
+print("value of z:",z)
+
+#Question 4
+print("Let's get started")
+
+#Question 5
+s="Acadview"
+course="Python"
+fees=5000
+print("%s %s fees is %d."%(s,course,fees))
+
+#Question 6
+name="Tony Stark"
+salary=1000000
+print("%s %d"%(name,salary))
